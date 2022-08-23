6d42c73dc0dd013eb267e37c3c2cd741f73f0937
Adriano Dias <adriano.rj.br@gmail.com>
2022-08-23 18:14:20 -0300

    cleanned some comments on controller file

--- src/main/java/com/adrianodias/testsslapi/controllers/UserController.java
+++ src/main/java/com/adrianodias/testsslapi/controllers/UserController.java
@@ -1,9 +1,6 @@
 package com.adrianodias.testsslapi.controllers;
 
 import java.util.List;
-//import java.util.Map;
-//import java.util.stream.Stream;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
